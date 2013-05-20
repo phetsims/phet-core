01539c3df0d1ac6989c76d415997961fd9af136c
Jonathan Olson <olsonsjc@gmail.com>
2013-05-20 12:05:19 -0600

    Updating copyright date

--- js/collect.js
+++ js/collect.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2012, University of Colorado
+// Copyright 2002-2013, University of Colorado
 
 /**
  * Creates an array of results from an iterator that takes a callback.
--- js/extend.js
+++ js/extend.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2012, University of Colorado
+// Copyright 2002-2013, University of Colorado
 
 /**
  * Like Underscore's _.extend, but with hardcoded support for ES5 getters/setters.
