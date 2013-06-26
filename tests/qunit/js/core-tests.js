c9082d289cc1f50f75f3439befcd17a194452d56
Sam Reid <reids@colorado.edu>
2013-06-25 22:37:23 -0600

    added semicolons and reformatted according to IDEA/PhetStyle.xml

--- tests/qunit/js/core-tests.js
+++ tests/qunit/js/core-tests.js
@@ -1,7 +1,6 @@
-
 (function() {
   module( 'Phet-core' );
-  
+
   test( 'isArray', function() {
     ok( core.isArray( [ 1, 2, 3 ] ) );
     ok( core.isArray( [] ) );
@@ -9,16 +8,15 @@
     ok( !core.isArray( {} ) );
     ok( !core.isArray( function() {} ) );
   } );
-  
+
   test( 'escapeHTML', function() {
     equal( core.escapeHTML( 'A&B' ), 'A&amp;B', 'simple &' );
-    equal( core.escapeHTML( 'A<B' ), 'A&lt;B', 'simple <' )
-    equal( core.escapeHTML( 'A>B' ), 'A&gt;B', 'simple >' )
-    equal( core.escapeHTML( 'A"B' ), 'A&quot;B', 'simple "' )
-    equal( core.escapeHTML( 'A\'B' ), 'A&#x27;B', 'simple \'' )
+    equal( core.escapeHTML( 'A<B' ), 'A&lt;B', 'simple <' );
+    equal( core.escapeHTML( 'A>B' ), 'A&gt;B', 'simple >' );
+    equal( core.escapeHTML( 'A"B' ), 'A&quot;B', 'simple "' );
+    equal( core.escapeHTML( 'A\'B' ), 'A&#x27;B', 'simple \'' );
     equal( core.escapeHTML( 'A/B' ), 'A&#x2F;B', 'simple /' );
-    
+
     equal( core.escapeHTML( '&amp; & ""' ), '&amp;amp; &amp; &quot;&quot;', 'multiple escaping' );
   } );
-  
 })();
