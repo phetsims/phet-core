ac56848a65a56b78dcde58fc1d00869b925fd17a
Jonathan Olson <olsonsjc@gmail.com>
2013-09-04 23:28:11 -0600

    Copying platform detection from joist's HomeScreen for refactoring

--- js/platform.js
+++ js/platform.js
@@ -10,11 +10,24 @@
  */
 define( function( require ) {
   'use strict';
-
+  
+  // taken from HomeScreen
+  function isIE( version ) {
+    var r = new RegExp( 'msie' + (!isNaN( version ) ? ('\\s' + version) : ''), 'i' );
+    return r.test( navigator.userAgent );
+  }
+  
   return {
     get firefox() { return navigator.userAgent.toLowerCase().indexOf( 'firefox' ) > -1; },
 
     //see http://stackoverflow.com/questions/3007480/determine-if-user-navigated-from-mobile-safari
-    get mobileSafari() { return navigator.userAgent.match( /(iPod|iPhone|iPad)/ ) && navigator.userAgent.match( /AppleWebKit/ );}
+    get mobileSafari() { return navigator.userAgent.match( /(iPod|iPhone|iPad)/ ) && navigator.userAgent.match( /AppleWebKit/ ); },
+    
+    get ie9() { return isIE( 9 ); },
+    get ie10() { return isIE( 10 ); },
+    get ie() { return navigator.userAgent.indexOf( 'MSIE'); },
+    
+    // from HomeScreen
+    get android() { return navigator.userAgent.indexOf( 'Android' ) > 0; }
   };
 } );
