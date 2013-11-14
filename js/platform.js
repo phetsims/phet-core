31549be40abeda91d44ef9446361d0b611121949
Jonathan Olson <olsonsjc@gmail.com>
2013-11-13 18:46:24 -0700

    Adding more platform support. Should be useful for https://github.com/phetsims/joist/issues/95

--- js/platform.js
+++ js/platform.js
@@ -13,24 +13,30 @@ define( function( require ) {
   
   var core = require( 'PHET_CORE/core' );
   
+  var ua = navigator.userAgent;
+  
   // taken from HomeScreen
   function isIE( version ) {
     var r = new RegExp( 'msie' + (!isNaN( version ) ? ('\\s' + version) : ''), 'i' );
-    return r.test( navigator.userAgent );
+    return r.test( ua );
   }
   
   var platform = core.platform = {
-    get firefox() { return navigator.userAgent.toLowerCase().indexOf( 'firefox' ) > -1; },
+    get firefox() { return ua.toLowerCase().indexOf( 'firefox' ) > -1; },
 
     //see http://stackoverflow.com/questions/3007480/determine-if-user-navigated-from-mobile-safari
-    get mobileSafari() { return navigator.userAgent.match( /(iPod|iPhone|iPad)/ ) && navigator.userAgent.match( /AppleWebKit/ ); },
+    get mobileSafari() { return ua.match( /(iPod|iPhone|iPad)/ ) && ua.match( /AppleWebKit/ ); },
+    get safari5() { return ua.match( /Version\/5\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ); },
+    get safari6() { return ua.match( /Version\/6\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ); },
+    get safari7() { return ua.match( /Version\/7\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ); },
     
     get ie9() { return isIE( 9 ); },
     get ie10() { return isIE( 10 ); },
-    get ie() { return navigator.userAgent.indexOf( 'MSIE' ) !== -1; },
+    get ie11() { return isIE( 11 ); },
+    get ie() { return ua.indexOf( 'MSIE' ) !== -1; },
     
     // from HomeScreen
-    get android() { return navigator.userAgent.indexOf( 'Android' ) > 0; }
+    get android() { return ua.indexOf( 'Android' ) > 0; }
   };
   return platform;
 } );
