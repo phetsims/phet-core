c626ec561fde53ce4f49df5b93ef3efca7cf1f08
Jonathan Olson <olsonsjc@gmail.com>
2015-01-20 13:19:45 -0700

    jshint-options => jshintOptions

--- tests/qunit/afterJSHintOptions.js
+++ tests/qunit/afterJSHintOptions.js
@@ -1,4 +1,4 @@
-window.jshintOptions = window.module.exports; // defined in jshint-options.js
+window.jshintOptions = window.module.exports; // defined in jshintOptions.js
 window.jshintGlobals = window.jshintOptions.globals;
 delete window.jshintOptions.globals; // jsHint live plugin wants globals to be separate, and we need to delete them from the options
 delete window.module; // don't leave node.js-like things sitting around
