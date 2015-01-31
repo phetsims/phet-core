20c96274346d9fafc1dca9e37051289ad837b859
Chris Malley <cmalley@pixelzoom.com>
2015-01-30 19:45:10 -0700

    fix breaking change to dot tests, phetsims/tasks#160

--- tests/qunit/afterJSHintOptions.js
+++ tests/qunit/afterJSHintOptions.js
@@ -1,4 +1,6 @@
+window.phet = window.phet || {};
+window.phet.phetCore = window.phet.phetCore || {};
 window.phet.phetCore.jshintOptions = window.module.exports; // defined in jshintOptions.js
 window.phet.phetCore.jshintGlobals = window.phet.phetCore.jshintOptions.globals;
-delete window.jshintOptions.globals; // jsHint live plugin wants globals to be separate, and we need to delete them from the options
+delete window.phet.phetCore.jshintOptions.globals; // jsHint live plugin wants globals to be separate, and we need to delete them from the options
 delete window.module; // don't leave node.js-like things sitting around
