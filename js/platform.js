328c6e2f6df1ee3da1bdd41877d2113cfc1e4a71
Jonathan Olson <olsonsjc@gmail.com>
2015-06-16 00:39:20 -0600

    Improved platform detection, see https://github.com/phetsims/phet-core/issues/20

--- js/platform.js
+++ js/platform.js
@@ -44,10 +44,10 @@ define( function( require ) {
     get firefox() { return ua.toLowerCase().indexOf( 'firefox' ) > -1; },
 
     //see http://stackoverflow.com/questions/3007480/determine-if-user-navigated-from-mobile-safari
-    get mobileSafari() { return ua.match( /(iPod|iPhone|iPad)/ ) && ua.match( /AppleWebKit/ ); },
-    get safari5() { return ua.match( /Version\/5\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ); },
-    get safari6() { return ua.match( /Version\/6\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ); },
-    get safari7() { return ua.match( /Version\/7\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ); },
+    get mobileSafari() { return !!( ua.match( /(iPod|iPhone|iPad)/ ) && ua.match( /AppleWebKit/ ) ); },
+    get safari5() { return !!( ua.match( /Version\/5\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ) ); },
+    get safari6() { return !!( ua.match( /Version\/6\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ) ); },
+    get safari7() { return !!( ua.match( /Version\/7\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ) ); },
 
     get ie9() { return isIE( 9 ); },
     get ie10() { return isIE( 10 ); },
@@ -57,7 +57,9 @@ define( function( require ) {
     // from HomeScreen
     get android() { return ua.indexOf( 'Android' ) > 0; },
 
-    get chromium() { return (/chrom(e|ium)/).test( ua.toLowerCase() ); }
+    get edge() { return !!ua.match( /Edge\// ); },
+
+    get chromium() { return (/chrom(e|ium)/).test( ua.toLowerCase() ) && !ua.match( /Edge\// ); }
   };
 
   return phetCore.platform;
