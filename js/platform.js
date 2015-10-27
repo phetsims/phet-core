6eab326e14a6dad382ac87ebc891696e03490c1a
Jonathan Olson <olsonsjc@gmail.com>
2015-10-27 11:29:49 -0600

    Don't recompute platform values, see https://github.com/phetsims/phet-core/issues/22

--- js/platform.js
+++ js/platform.js
@@ -42,33 +42,33 @@ define( function( require ) {
 
   phetCore.platform = {
     // Whether the browser is most likely Firefox
-    get firefox() { return ua.toLowerCase().indexOf( 'firefox' ) > -1; },
+    firefox: ua.toLowerCase().indexOf( 'firefox' ) > -1,
 
     // Whether the browser is most likely Safari running on iOS
     // See http://stackoverflow.com/questions/3007480/determine-if-user-navigated-from-mobile-safari
-    get mobileSafari() { return !!( ua.match( /(iPod|iPhone|iPad)/ ) && ua.match( /AppleWebKit/ ) ); },
+    mobileSafari: !!( ua.match( /(iPod|iPhone|iPad)/ ) && ua.match( /AppleWebKit/ ) ),
 
     // Whether the browser is a matching version of Safari running on OS X
-    get safari5() { return !!( ua.match( /Version\/5\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ) ); },
-    get safari6() { return !!( ua.match( /Version\/6\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ) ); },
-    get safari7() { return !!( ua.match( /Version\/7\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ) ); },
+    safari5: !!( ua.match( /Version\/5\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ) ),
+    safari6: !!( ua.match( /Version\/6\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ) ),
+    safari7: !!( ua.match( /Version\/7\./ ) && ua.match( /Safari\// ) && ua.match( /AppleWebKit/ ) ),
 
     // Whether the browser is some type of IE (Internet Explorer)
-    get ie() { return getInternetExplorerVersion() !== -1; },
+    ie: getInternetExplorerVersion() !== -1,
 
     // Whether the browser is a specific version of IE (Internet Explorer)
-    get ie9() { return isIE( 9 ); },
-    get ie10() { return isIE( 10 ); },
-    get ie11() { return isIE( 11 ); },
+    ie9: isIE( 9 ),
+    ie10: isIE( 10 ),
+    ie11: isIE( 11 ),
 
     // Whether the browser has Android in its user agent
-    get android() { return ua.indexOf( 'Android' ) > 0; },
+    android: ua.indexOf( 'Android' ) > 0,
 
     // Whether the browser is Microsoft Edge
-    get edge() { return !!ua.match( /Edge\// ); },
+    edge: !!ua.match( /Edge\// ),
 
     // Whether the browser is Chromium-based (usually Chrome)
-    get chromium() { return (/chrom(e|ium)/).test( ua.toLowerCase() ) && !ua.match( /Edge\// ); }
+    chromium: (/chrom(e|ium)/).test( ua.toLowerCase() ) && !ua.match( /Edge\// )
   };
 
   return phetCore.platform;
