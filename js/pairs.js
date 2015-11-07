daa836f4aca6f415c687176bc9053ba311b818a3
samreid <reids@colorado.edu>
2015-11-06 20:50:40 -0700

    updated copyright info using grunt update-copyright-dates, see  https://github.com/phetsims/chipper/issues/403

--- js/pairs.js
+++ js/pairs.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2014-2015, University of Colorado Boulder
 
 /**
  * Creates an array of arrays, which consists of pairs of objects from the input array without duplication.
--- js/phetAllocation.js
+++ js/phetAllocation.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2013-2015, University of Colorado Boulder
 
 /**
  * Object instance allocation tracking, so we can cut down on garbage collection.
