352ffb3be3d428c744df61d695cd867181617a1e
Jonathan Olson <olsonsjc@gmail.com>
2015-11-09 16:08:07 -0700

    Supporting dot syntax for Namespace.register, see https://github.com/phetsims/tasks/issues/378

--- js/Namespace.js
+++ js/Namespace.js
@@ -28,13 +28,44 @@ define( function( require ) {
 
     /**
      * Registers a key-value pair with the namespace.
+     *
+     * If there are no dots ('.') in the key, it will be assigned to the namespace. For example:
+     * - x.register( 'A', A );
+     * will set x.A = A.
+     *
+     * If the key contains one or more dots ('.'), it's treated somewhat like a path expression. For instance, if the
+     * following is called:
+     * - x.register( 'A.B.C', C );
+     * then the register function will navigate to the object x.A.B and add x.A.B.C = C.
+     *
      * @param {string} key
      * @param {*} value
      * @public
      */
     register: function( key, value ) {
-      assert && assert( !this[ key ], key + ' is already registered for namespace ' + this.name );
-      this[ key ] = value;
+      // If the key isn't compound (doesn't contain '.'), we can just look it up on this namespace
+      if ( key.indexOf( '.' ) < 0 ) {
+        assert && assert( !this[ key ], key + ' is already registered for namespace ' + this.name );
+        this[ key ] = value;
+      }
+      // Compound (contains '.' at least once). x.register( 'A.B.C', C ) should set x.A.B.C.
+      else {
+        var keys = key.split( '.' ); // e.g. [ 'A', 'B', 'C' ]
+
+        // Walk into the namespace, verifying that each level exists. e.g. parent => x.A.B
+        var parent = this;
+        for ( var i = 0; i < keys.length - 1; i++ ) { // for all but the last key
+          assert && assert( !!parent[ keys[ i ] ],
+            [ this.name ].concat( keys.slice( 0, i + 1 ) ).join( '.' ) + ' needs to be defined to register ' + key );
+
+          parent = parent[ keys[ i ] ];
+        }
+
+        // Write into the inner namespace, e.g. x.A.B[ 'C' ] = C
+        var lastKey = keys[ keys.length - 1 ];
+        assert && assert( !parent[ lastKey ], key + ' is already registered for namespace ' + this.name );
+        parent[ lastKey ] = value;
+      }
     }
   } );
 } );
