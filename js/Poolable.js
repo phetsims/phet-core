d2cd9a63a541608e3b18f1e11e3c5e54e48c7781
Jesse <jesse.greenberg@colorado.edu>
2019-02-11 13:31:41 -0500

    trait -> mixin in documentation after discussion in phetsims/phet-info#92

--- js/Poolable.js
+++ js/Poolable.js
@@ -1,7 +1,7 @@
 // Copyright 2015-2018, University of Colorado Boulder
 
 /**
- * Object pooling trait, for cases where creating new objects is expensive, and we'd rather mark some objects as able
+ * Object pooling mixin, for cases where creating new objects is expensive, and we'd rather mark some objects as able
  * to be reused (i.e. 'in the pool'). This provides a pool of objects for each type it is invoked on. It allows for
  * getting "new" objects that can either be constructed OR pulled in from a pool, and requires that the objects are
  * essentially able to "re-run" the constructor. Then when putting the object back in the pool, references should be
