ec75d4972ed6ddcbfbc8cdc32647a334ee540c27
zepumph <michael.kauzmann@colorado.edu>
2019-09-12 14:36:06 -0800

    fix no-needless-escape eslint rule and turn it on, https://github.com/phetsims/chipper/issues/750

--- js/escapeHTML.js
+++ js/escapeHTML.js
@@ -19,8 +19,8 @@ define( function( require ) {
       .replace( /&/g, '&amp;' )
       .replace( /</g, '&lt;' )
       .replace( />/g, '&gt;' )
-      .replace( /\"/g, '&quot;' )
-      .replace( /\'/g, '&#x27;' )
+      .replace( /"/g, '&quot;' )
+      .replace( /'/g, '&#x27;' )
       .replace( /\//g, '&#x2F;' );
   }
 
