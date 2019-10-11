ed3bb6bc8dd74666b7493f1258024ba900c190de
zepumph <michael.kauzmann@colorado.edu>
2019-10-10 21:19:08 -0800

    update merge documentation, https://github.com/phetsims/phet-core/issues/71

--- js/merge.js
+++ js/merge.js
@@ -4,8 +4,8 @@
  * Like Lodash's _.merge, this will recursively merge nested options objects provided that the keys end in 'Options'
  * (case sensitive) and they are pure object literals.
  * That is, they must be defined by `... = { ... }` or `somePropOptions: { ... }`.
- * Non object literals (arrays, functions, and inherited types) will all throw assertion errors if passed in as an arg
- * or as a value to a `*Options` property.
+ * Non object literals (arrays, functions, and inherited types) or anything with an extra prototype will all throw
+ * assertion errors if passed in as an arg or as a value to a `*Options` field.
  *
  * @author Michael Barlow (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
