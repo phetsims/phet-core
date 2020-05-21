2261ce2608590bed0d6583c1e0b2c423effa48f9
samreid <reids@colorado.edu>
2020-05-21 14:54:15 -0600

    Use qunitStart, see https://github.com/phetsims/aqua/issues/89

--- js/phet-core-tests.js
+++ js/phet-core-tests.js
@@ -26,6 +26,7 @@ import './mixedWithTests.js';
 import './pairsTests.js';
 import './partitionTests.js';
 import './swapObjectKeysTests.js';
+import qunitStart from '../../chipper/js/sim-tests/qunitStart.js';
 
 // Since our tests are loaded asynchronously, we must direct QUnit to begin the tests
-QUnit.start();
+qunitStart();
