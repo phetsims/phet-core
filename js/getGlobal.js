6b904583f3b520bd32cb30b8e54906d4b7e43118
zepumph <michael.kauzmann@colorado.edu>
2021-01-27 16:40:50 -0900

    create getGlobal, and use it in MenuItem, https://github.com/phetsims/joist/issues/686

---
+++ js/getGlobal.js
@@ -0,0 +1,25 @@
+// Copyright 2021, University of Colorado Boulder
+
+/**
+ * Support gracefully getting a global object to itself. Returns null if the global doesn't exist.
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import phetCore from './phetCore.js';
+
+/**
+ * If the path exists on the window global, return it, otherwise returns null
+ * @param {string} path a path to global, sucha s 'phet.joist.sim'
+ * @returns {function|null}
+ */
+const getGlobal = path => {
+  assert && assert( typeof path === 'string', 'path must be a string' );
+  assert && assert( path.trim() === path, 'path must be trimmed' );
+  const global = _.get( window, path );
+  return global !== undefined ? global : null;
+};
+
+phetCore.register( 'getGlobal', getGlobal );
+
+export default getGlobal;
