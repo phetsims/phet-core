2ba44464e7a13df70a30e9ddfedb65c0eca23eaa
Sam Reid <reids@colorado.edu>
2021-11-06 20:44:01 -0600

    Add Orientation.d.ts, see https://github.com/phetsims/phet-core/issues/93

---
+++ js/Orientation.d.ts
@@ -0,0 +1,12 @@
+// Copyright 2021, University of Colorado Boulder
+
+/**
+ * Provides TypeScript types until we can put the core file in TypeScript directly.
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+class Orientation {
+  static HORIZONTAL: Orientation
+  static VERTICAL: Orientation
+}
+
+export default Orientation;
