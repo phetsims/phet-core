2e3db09a3e92a65d4998b79190850b6dc58cae00
Sam Reid <reids@colorado.edu>
2022-01-05 11:02:56 -0700

    Add EnumerationValue.isEnumerationValue and specify subtypes in RichEnumeration classes, see https://github.com/phetsims/chipper/issues/1106

--- js/EnumerationValue.ts
+++ js/EnumerationValue.ts
@@ -1,9 +1,11 @@
 // Copyright 2021, University of Colorado Boulder
 
 /**
+ * Base type for enumeration value instances.
+ *
  * @author Sam Reid (PhET Interactive Simulations)
+ * @author Michael Kauzmann (PhET Interactive Simulations)
  */
-
 import phetCore from './phetCore.js';
 
 class EnumerationValue {
@@ -12,6 +14,10 @@ class EnumerationValue {
   toString() {
     return this.name;
   }
+
+  // This method is unused, but needs to remain here so other types don't accidentally structurally match
+  // enumeration values.  Without this, string satisfies the EnumerationValue interface, but we don't want it to.
+  isEnumerationValue() {return true;}
 }
 
 phetCore.register( 'EnumerationValue', EnumerationValue );
