f0850546640edd5346b9ec520c1f77a71361bfd6
phet-dev <phetadmin@gmail.com>
2022-01-06 03:19:47 -0700

    update copyright dates from daily grunt work

--- js/RichEnumeration.ts
+++ js/RichEnumeration.ts
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * This implementation auto-detects the enumeration values by Object.keys and instanceof.  Every property that has a
