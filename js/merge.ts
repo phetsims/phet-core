adc3996dbfa2d0e13002f5cc82561407a158ed93
zepumph <michael.kauzmann@colorado.edu>
2022-01-13 17:43:21 -0700

    convert merge to typescript, https://github.com/phetsims/chipper/issues/1128

--- js/merge.ts
+++ js/merge.ts
@@ -16,12 +16,28 @@ import phetCore from './phetCore.js';
 // constants
 const OPTIONS_SUFFIX = 'Options';
 
+// Function overloading is described in https://www.tutorialsteacher.com/typescript/function-overloading
+function merge<A, B>( a: A, b: B ): A & B;
+function merge<A, B, C>( a: A, b: B, c: C ): A & B & C; // eslint-disable-line no-redeclare
+function merge<A, B, C, D>( a: A, b: B, c: C, d: D ): A & B & C & D; // eslint-disable-line no-redeclare
+function merge<A, B, C, D, E>( a: A, b: B, c: C, d: D, e: E ): A & B & C & D & E; // eslint-disable-line no-redeclare
+
+// TODO: SR and MK tried two other potential ways of typing out Merge, below we explain the issues associated with both, https://github.com/phetsims/chipper/issues/1128
+// https://dev.to/vborodulin/ts-how-to-override-properties-with-type-intersection-554l
+// type Override<T1, T2> = Omit<T1, keyof T2> & T2;
+
+// TODO: This is incorrect because it doesn't adequately recognize when A fills in a default for an optional option that B thinks could be undefined, https://github.com/phetsims/chipper/issues/1128
+// function merge<A, B>( a: A, b: B ): Override<A, B>;
+
+// TODO: This is incorrect because it results in false positives as it assumes that everything optional in B has had defaults filled in by A, even if the values actually weren't filled in by A, https://github.com/phetsims/chipper/issues/1128
+// function merge<A, B>( a: A, b: B ): Required<Override<A, B>>;
+
 /**
  * @param  {Object} target - the object literal that will have keys set to it
  * @param  {...<Object|null>} sources
  * @returns {Object}
  */
-function merge( target, ...sources ) {
+function merge( target: any, ...sources: any ) { // eslint-disable-line no-redeclare
   assert && assertIsMergeable( target );
   assert && assert( target !== null, 'target should not be null' ); // assertIsMergeable supports null
   assert && assert( sources.length > 0, 'at least one source expected' );
@@ -51,22 +67,23 @@ function merge( target, ...sources ) {
 }
 
 /**
+ * TODO: can we remove assertIsMergeable? https://github.com/phetsims/chipper/issues/1128
  * Asserts that the object is compatible with merge. That is, it's a POJSO.
  * This function must be called like: assert && assertIsMergeable( arg );
  * @param {Object|null} object
  */
-function assertIsMergeable( object ) {
-  assert( object === null ||
-          ( object && typeof object === 'object' && Object.getPrototypeOf( object ) === Object.prototype ),
+function assertIsMergeable( object: any ) {
+  assert && assert( object === null ||
+                    ( object && typeof object === 'object' && Object.getPrototypeOf( object ) === Object.prototype ),
     'object is not compatible with merge' );
 
   if ( object !== null ) {
     // ensure that options keys are not ES5 setters or getters
     Object.keys( object ).forEach( prop => {
-      const ownPropertyDescriptor = Object.getOwnPropertyDescriptor( object, prop );
-      assert( !ownPropertyDescriptor.hasOwnProperty( 'set' ),
+      const ownPropertyDescriptor = Object.getOwnPropertyDescriptor( object, prop )!;
+      assert && assert( !ownPropertyDescriptor.hasOwnProperty( 'set' ),
         'cannot use merge with an object that has a setter' );
-      assert( !ownPropertyDescriptor.hasOwnProperty( 'get' ),
+      assert && assert( !ownPropertyDescriptor.hasOwnProperty( 'get' ),
         'cannot use merge with an object that has a getter' );
     } );
   }
