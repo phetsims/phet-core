91f432a2b59b628259886670dfe60c201094f739
Sam Reid <reids@colorado.edu>
2022-01-18 11:42:09 -0700

    Add TODOs, see https://github.com/phetsims/chipper/issues/1128

--- js/optionize.ts
+++ js/optionize.ts
@@ -27,7 +27,8 @@ type Options<T> = Pick<T, OptionalKeys<T>>;
 // an object with filled in defaults, and all the required things.  In our level and parent levels.
 
 // TODO: How can we indicate that a required parameter will come in through s or a but not both?  See https://github.com/phetsims/chipper/issues/1128
-
+// TODO: Do we have to put the A all type parameter?  Could it be omitted or inferred?  See https://github.com/phetsims/chipper/issues/1128
+// TODO: See https://github.com/phetsims/chipper/issues/1128 Perhaps not inferred, since it may Omit things
 // TypeScript is all-or-none on inferring generic parameter types (per function), so we must use the nested strategy in
 // https://stackoverflow.com/questions/63678306/typescript-partial-type-inference to specify the types we want
 // while still allowing definitions to flow through.
