3b64b403c9a812b63902021f5e5800057005caed
zepumph <michael.kauzmann@colorado.edu>
2022-01-19 09:47:19 -0700

    investigate defaults for parent-required args (limitation I), https://github.com/phetsims/chipper/issues/1128

--- js/optionize.ts
+++ js/optionize.ts
@@ -1,7 +1,6 @@
 // Copyright 2022, University of Colorado Boulder
 
 import phetCore from './phetCore.js';
-
 import merge from './merge.js';
 
 // https://github.com/piotrwitek/utility-types/blob/master/src/mapped-types.ts
@@ -26,26 +25,50 @@ type Options<T> = Pick<T, OptionalKeys<T>>;
 // what comes out of merge?
 // an object with filled in defaults, and all the required things.  In our level and parent levels.
 
-// TODO: How can we indicate that a required parameter will come in through s or a but not both?  See https://github.com/phetsims/chipper/issues/1128
-// TODO: Do we have to put the A all type parameter?  Could it be omitted or inferred?  See https://github.com/phetsims/chipper/issues/1128
-// TODO: See https://github.com/phetsims/chipper/issues/1128 Perhaps not inferred, since it may Omit things
-// TypeScript is all-or-none on inferring generic parameter types (per function), so we must use the nested strategy in
-// https://stackoverflow.com/questions/63678306/typescript-partial-type-inference to specify the types we want
-// while still allowing definitions to flow through.
+// TODO: (I) How can we indicate that a required parameter (for P) will come in through defaults and/or providedOptions? Note: required parameters for S will not come from defaults.  See https://github.com/phetsims/chipper/issues/1128
 // S = SubclassSelfOptions
 // P = ParentOptions
 // A = AllSubclassProvidedOptions
 // M = list of keys that are used in the constructor
-const optionize = <S, P = {}, M extends keyof P = never, A = S & P>( s: Required<Options<S>> & Partial<P> & Required<Pick<P, M>>, a?: A ) => {
-  return merge( s, a );
+const optionize = <S, P = {}, M extends keyof P = never, A = S & P>( defaults: Required<Options<S>> & Partial<P> & Required<Pick<P, M>>, providedOptions?: A ) => {
+  return merge( defaults, providedOptions );
 };
 
+// TypeScript is all-or-none on inferring generic parameter types (per function), so we must use the nested strategy in
+// https://stackoverflow.com/questions/63678306/typescript-partial-type-inference to specify the types we want
+// while still allowing definitions to flow through.
 // This also works, we will keep it here now in case it helps with further improvements with inference.
 // const optionize = <S, P, M extends keyof P = never>() => {
-//   return <B>( s: Required<Options<S>> & Partial<P> & Required<Pick<P, M>>, a?: B ) => {
-//     return merge( s, a );
+//   return <B>( defaults: Required<Options<S>> & Partial<P> & Required<Pick<P, M>>, providedOptions?: B ) => {
+//     return merge( defaults, providedOptions );
 //   };
 // };
+
+/*
+Limitation (I):
+
+This gets us half way there, when you have required args to the parent, this makes sure that you don't make
+providedOptions optional (with a question mark). We still need a way to note when the required param is specified via the self options.
+const optionize = <S, P = {}, M extends keyof P = never, A = S & P>(
+  defaults: Required<Options<S>> & Partial<P> & Required<Pick<P, M>>,
+  providedOptions: RequiredKeys<A> extends never ? ( A | undefined ) : A
+) => {
+  return merge( defaults, providedOptions );
+};
+
+TEST TO SEE IF WE CAN GET TYPESCRIPT TO KNOW ABOUT REQUIRED ARGUMENTS TO POTENTIALLY COME FROM EITHER ARG.
+const optionize = <S, P = {}, M extends keyof P = never, A = S & P>() => {
+  type FirstArg = Required<Options<S>> & Partial<P> & Required<Pick<P, M>>;
+  return (
+    defaults: FirstArg,
+    //NOT WORKING: If any required elements were in the first arg, then we don't need them here, and potentially can mark providedOptions as a whole as optional
+    providedOptions: RequiredKeys<FirstArg> extends never ? RequiredKeys<A> extends never ? ( A | undefined ) : A : A
+  ) => {
+    return merge( defaults, providedOptions );
+  };
+};
+ */
+
 
 phetCore.register( 'optionize', optionize );
 export default optionize;
