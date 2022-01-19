dfca83f5d51dd6bd26c54b96e46961c0ef072a9a
zepumph <michael.kauzmann@colorado.edu>
2022-01-19 15:13:05 -0700

    name generic parameters, https://github.com/phetsims/chipper/issues/1128

--- js/optionize.ts
+++ js/optionize.ts
@@ -25,14 +25,22 @@ type Options<T> = Pick<T, OptionalKeys<T>>;
 // what comes out of merge?
 // an object with filled in defaults, and all the required things.  In our level and parent levels.
 
-// TODO: (I) How can we indicate that a required parameter (for P) will come in through defaults and/or providedOptions? Note: required parameters for S will not come from defaults.  See https://github.com/phetsims/chipper/issues/1128
-// S = SubclassSelfOptions
-// P = ParentOptions
-// M = list of keys from ParentOptions that are used in the constructor
-// A = AllSubclassProvidedOptions
-const optionize = <S, P = {}, M extends keyof P = never, A = S & P>( defaults: Required<Options<S>> & Partial<P> & Required<Pick<P, M>>, providedOptions?: A ) => {
-  return merge( defaults, providedOptions );
-};
+// TODO: (I) How can we indicate that a required parameter (for ParentOptions) will come in through defaults and/or providedOptions? Note: required parameters for S will not come from defaults.  See https://github.com/phetsims/chipper/issues/1128
+// SelfOptions = SubclassSelfOptions
+// ParentOptions = ParentOptions
+// KeysUsedInSubclassConstructor = list of keys from ParentOptions that are used in the constructor
+// ProvidedOptions = AllSubclassProvidedOptions
+const optionize =
+  <SelfOptions,
+    ParentOptions = {},
+    KeysUsedInSubclassConstructor extends keyof ParentOptions = never,
+    ProvidedOptions = SelfOptions & ParentOptions>
+  (
+    defaults: Required<Options<SelfOptions>> & Partial<ParentOptions> & Required<Pick<ParentOptions, KeysUsedInSubclassConstructor>>,
+    providedOptions?: ProvidedOptions
+  ) => {
+    return merge( defaults, providedOptions );
+  };
 
 // TypeScript is all-or-none on inferring generic parameter types (per function), so we must use the nested strategy in
 // https://stackoverflow.com/questions/63678306/typescript-partial-type-inference to specify the types we want
