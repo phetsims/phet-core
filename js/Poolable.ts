e259857c3f1ea0789551d94fe5f326628c384eb3
phet-dev <phetadmin@gmail.com>
2022-02-09 03:28:13 -0700

    update copyright dates from daily grunt work

--- js/Poolable.ts
+++ js/Poolable.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2021, University of Colorado Boulder
+// Copyright 2015-2022, University of Colorado Boulder
 
 /**
  * Object pooling mixin, for cases where creating new objects is expensive, and we'd rather mark some objects as able
