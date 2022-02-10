108123565fe92f2fe03ce69918243b55e74c1083
zepumph <michael.kauzmann@colorado.edu>
2022-02-10 13:41:04 -0700

    add IntentionalAny.ts, https://github.com/phetsims/tasks/issues/1096

---
+++ js/IntentionalAny.ts
@@ -0,0 +1,11 @@
+// Copyright 2022, University of Colorado Boulder
+
+/**
+ * to specify when `any` is actually the best type for the case (albeit slightly unfortunate). This is not a to-do
+ * to be removed.
+ *
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+type IntentionalAny = any;
+export default IntentionalAny;
