ccf50b00d194a98e4f94003290db1d026029b9a8
Jonathan Olson <jonathan.olson@colorado.edu>
2022-02-23 19:04:37 -0700

    Display/Input/Pointers/SceneryEvent/Overlays/Timer => TypeScript, added Poolable2, and relevant patches based on types

--- js/Constructor.ts
+++ js/Constructor.ts
@@ -1,10 +1,12 @@
 // Copyright 2022, University of Colorado Boulder
 
+import IntentionalAny from './IntentionalAny.js';
+
 /**
  * Simple generic Type for a Constructor.
  *
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
-type Constructor<T = {}> = new ( ...args: any[] ) => T;
+type Constructor<T = {}, K extends IntentionalAny[] = IntentionalAny[]> = new ( ...args: K ) => T;
 export default Constructor;
--- js/ConstructorOf.ts
+++ js/ConstructorOf.ts
@@ -1,10 +1,12 @@
 // Copyright 2022, University of Colorado Boulder
 
+import IntentionalAny from './IntentionalAny.js';
+
 /**
  * Converts an instance type to a constructor type.
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
-type ConstructorOf<T> = { new( ...args: any[] ): T; };
+type ConstructorOf<T> = { new( ...args: IntentionalAny[] ): T; };
 export default ConstructorOf;
