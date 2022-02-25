925dd4d0fcf5301516c0e667838ae00aa18b4247
pixelzoom <cmalley@pixelzoom.com>
2022-02-25 12:09:20 -0700

    doc

--- js/types/PickOptional.ts
+++ js/types/PickOptional.ts
@@ -1,8 +1,9 @@
 // Copyright 2022, University of Colorado Boulder
 
 /**
- * Use PickRequired to pick properties of a type T and make them optional.
+ * Use PickOptional to pick properties of a type T and make them optional.
  * This is useful when picking superclass options that you want to expose in a subclass API.
+ * (Careful if you pick a required superclass option and make it optional - you'll need to provide a default!)
  * It makes life a little easier because you have to fiddle with fewer '<' and '>' characters,
  * and PickOptional makes a little more sense than Pick<Partial> in the context of options.
  *
