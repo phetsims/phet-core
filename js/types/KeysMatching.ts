8af61038640a2144fc64d2dfe774f6436bfa375d
Jonathan Olson <jonathan.olson@colorado.edu>
2022-03-07 12:47:13 -0700

    DynamicProperty => TypeScript + improving DragListener end consistency with an event. See https://github.com/phetsims/scenery-phet/issues/726

---
+++ js/types/KeysMatching.ts
@@ -0,0 +1,13 @@
+// Copyright 2022, University of Colorado Boulder
+
+/**
+ * Finds keys of an object that match a specific type, see
+ * https://stackoverflow.com/questions/54520676/in-typescript-how-to-get-the-keys-of-an-object-type-whose-values-are-of-a-given
+ *
+ * For instance, KeysMatching<{ foo: number, x: string, k: number }, number> will be 'foo' | 'k'
+ *
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+
+type KeysMatching<T, V> = {[K in keyof T]-?: T[K] extends V ? K : never}[keyof T];
+export default KeysMatching;
