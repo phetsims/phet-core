73efacbf79eb7515c654406872a33c8cdd736cbd
Sam Reid <reids@colorado.edu>
2022-04-28 10:11:03 -0600

    Add identity, see https://github.com/phetsims/phet-core/issues/114

---
+++ js/identity.ts
@@ -0,0 +1,16 @@
+// Copyright 2018-2020, University of Colorado Boulder
+
+/**
+ * Function that returns its input. This was added as an alternative to _.identity because WebStorm did
+ * not provide as good navigation for _.identity.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+
+import phetCore from './phetCore.js';
+
+export default function identity<T>( t: T ): T {
+  return t;
+}
+
+phetCore.register( 'identity', identity );
