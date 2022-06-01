a30089d930fa8824b4e837b75c07a6a4b5f8c036
pixelzoom <cmalley@pixelzoom.com>
2022-06-01 08:32:54 -0600

    imports after doc

--- js/types/ConstructorOf.ts
+++ js/types/ConstructorOf.ts
@@ -1,12 +1,12 @@
 // Copyright 2022, University of Colorado Boulder
 
-import IntentionalAny from './IntentionalAny.js';
-
 /**
  * Converts an instance type to a constructor type.
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
+
+import IntentionalAny from './IntentionalAny.js';
 
 type ConstructorOf<T> = { new( ...args: IntentionalAny[] ): T };
 export default ConstructorOf;
