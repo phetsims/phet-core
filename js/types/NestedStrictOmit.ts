5fae804205d40820340a4ae70d834e00dbd454a7
pixelzoom <cmalley@pixelzoom.com>
2022-06-29 10:33:09 -0600

    use SR's implementation of NestedStrictOmit, https://github.com/phetsims/scenery-phet/issues/730

--- js/types/NestedStrictOmit.ts
+++ js/types/NestedStrictOmit.ts
@@ -5,7 +5,6 @@
  *
  * Example: NestedStrictOmit<NumberControl, 'sliderOptions', 'trackNode' | 'thumbNode'>
  *
- * @author Chris Malley (PixelZoom, Inc.)
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
@@ -16,8 +15,8 @@ import StrictOmit from './StrictOmit.js';
  * @param P - the name of the property containing the nested properties
  * @param K - the keys of the nested properties to be omitted
  */
+type Omit2<T, P extends keyof T, K extends keyof Required<T>[P]> = Record<P, StrictOmit<Required<T>[P], K>>;
 type NestedStrictOmit<T, P extends keyof T, K extends keyof Required<T>[P]> =
-  StrictOmit<T, P> &
-  Partial<Record<P, StrictOmit<Required<T>[P], K>>>;
+  StrictOmit<T, P> & ( undefined extends T[P] ? Partial<Omit2<T, P, K>> : Omit2<T, P, K> );
 
 export default NestedStrictOmit;
