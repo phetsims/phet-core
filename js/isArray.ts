fd1f75e9a311d0c2cdbd6e756ce0644fa2a315eb
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-03 13:39:09 -0600

    Screen-related work for https://github.com/phetsims/joist/issues/827

--- js/isArray.ts
+++ js/isArray.ts
@@ -7,8 +7,9 @@
  */
 
 import phetCore from './phetCore.js';
+import IntentionalAny from './types/IntentionalAny.js';
 
-function isArray( array ) {
+function isArray( array: IntentionalAny ): array is IntentionalAny[] {
   // yes, this is actually how to do this. see http://stackoverflow.com/questions/4775722/javascript-check-if-object-is-array
   return Object.prototype.toString.call( array ) === '[object Array]';
 }
