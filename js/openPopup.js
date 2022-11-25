e180917d4f938d15cc122dbd2534f2a4c60a2d0e
Jonathan Olson <jonathan.olson@colorado.edu>
2022-11-25 09:02:33 -0700

    Preventing openPopup from opening links if allowLinks is false, see https://github.com/phetsims/joist/issues/830

--- js/openPopup.js
+++ js/openPopup.js
@@ -12,14 +12,21 @@ import phetCore from './phetCore.js';
  * Opens the URL in a new window or tab.
  *
  * @param {string} url
+ * @param {boolean} [allowPopup] - Whether to allow a popup window. If false, will open in a new tab.
  */
-function openPopup( url ) {
-  const popupWindow = window.open( url, '_blank' ); // open in a new window/tab
+function openPopup( url, allowPopup ) {
+  if ( allowPopup === undefined ) {
+    allowPopup = !window.phet || !phet.chipper || !phet.chipper.queryParameters || phet.chipper.queryParameters.allowLinks;
+  }
 
-  // We can't guarantee the presence of a window object, since if it isn't opened then it will return null.
-  // See https://github.com/phetsims/phet-ios-app/issues/508#issuecomment-520891177 and documentation at
-  // https://developer.mozilla.org/en-US/docs/Web/API/Window/open.
-  popupWindow && popupWindow.focus();
+  if ( allowPopup ) {
+    const popupWindow = window.open( url, '_blank' ); // open in a new window/tab
+
+    // We can't guarantee the presence of a window object, since if it isn't opened then it will return null.
+    // See https://github.com/phetsims/phet-ios-app/issues/508#issuecomment-520891177 and documentation at
+    // https://developer.mozilla.org/en-US/docs/Web/API/Window/open.
+    popupWindow && popupWindow.focus();
+  }
 }
 
 phetCore.register( 'openPopup', openPopup );
