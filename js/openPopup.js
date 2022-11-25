32dc878b8b101bac416eab7df05fc315be20a388
Jonathan Olson <jonathan.olson@colorado.edu>
2022-11-25 09:13:06 -0700

    Preventing openPopup from opening links if allowLinks is false, see https://github.com/phetsims/joist/issues/830

--- js/openPopup.js
+++ js/openPopup.js
@@ -16,13 +16,18 @@ define( require => {
    *
    * @param {string} url
    */
-  function openPopup( url ) {
-    const popupWindow = window.open( url, '_blank' ); // open in a new window/tab
+  function openPopup( url, allowPopup ) {
+    if ( allowPopup === undefined ) {
+      allowPopup = !window.phet || !phet.chipper || !phet.chipper.queryParameters || phet.chipper.queryParameters.allowLinks;
+    }
+    if ( allowPopup ) {
+      const popupWindow = window.open( url, '_blank' ); // open in a new window/tab
 
-    // We can't guarantee the presence of a window object, since if it isn't opened then it will return null.
-    // See https://github.com/phetsims/phet-ios-app/issues/508#issuecomment-520891177 and documentation at
-    // https://developer.mozilla.org/en-US/docs/Web/API/Window/open.
-    popupWindow && popupWindow.focus();
+      // We can't guarantee the presence of a window object, since if it isn't opened then it will return null.
+      // See https://github.com/phetsims/phet-ios-app/issues/508#issuecomment-520891177 and documentation at
+      // https://developer.mozilla.org/en-US/docs/Web/API/Window/open.
+      popupWindow && popupWindow.focus();
+    }
   }
 
   return phetCore.register( 'openPopup', openPopup );
