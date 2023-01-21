f39a89fdf58c5e1e5614bb596427d55a85b8b67f
Jonathan Olson <jonathan.olson@colorado.edu>
2023-01-21 12:11:38 -0700

    Some twixt TypeScript conversions, adding WritableKeys/ReadonlyKeys, see https://github.com/phetsims/sun/issues/814

---
+++ js/types/ReadonlyKeys.ts
@@ -0,0 +1,19 @@
+// Copyright 2022, University of Colorado Boulder
+
+/**
+ * Returns the writable keys of an object type.
+ *
+ * See https://stackoverflow.com/questions/49579094/typescript-conditional-types-filter-out-readonly-properties-pick-only-requir
+ *
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+
+// NOTE: Did not refactor out this IfEquals, because it doesn't handle all cases correctly
+type IfEquals<X, Y, A=X, B=never> =
+  ( <T>() => T extends X ? 1 : 2 ) extends
+  ( <T>() => T extends Y ? 1 : 2 ) ? A : B;
+
+type ReadonlyKeys<T> = {
+  [P in keyof T]-?: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, never, P>
+}[keyof T];
+export default ReadonlyKeys;
