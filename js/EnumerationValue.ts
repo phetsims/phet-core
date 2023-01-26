e79b6f57cd34418219fd79c3ce45f6f3d0fa8d46
phet-dev <phetadmin@gmail.com>
2023-01-26 04:48:17 -0700

    update copyright dates from daily grunt work

--- js/EnumerationValue.ts
+++ js/EnumerationValue.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 /**
  * EnumerationValue is the base class for enumeration value instances.
