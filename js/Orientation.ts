6910a9200322573984706333ef9a1f9a23f120d3
phet-dev <phetadmin@gmail.com>
2023-01-28 04:44:13 -0700

    update copyright dates from daily grunt work

--- js/Orientation.ts
+++ js/Orientation.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * Either horizontal or vertical, with helper values.  Moved from Area Model Common on Nov 7, 2019
