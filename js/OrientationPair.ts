7b412e911a9f07596824e4cbf096b95295594484
phet-dev <phetadmin@gmail.com>
2023-08-08 03:34:58 -0600

    update copyright dates from daily grunt work

--- js/OrientationPair.ts
+++ js/OrientationPair.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 /**
  * An object that contains a value for each item in an enumeration.
