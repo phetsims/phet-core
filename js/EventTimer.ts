e156ec71138a3533a18092ef3c5826767a44d5a9
phet-dev <phetadmin@gmail.com>
2024-04-30 03:23:51 -0600

    update copyright dates from daily grunt work

--- js/EventTimer.ts
+++ js/EventTimer.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2023, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  * Abstraction for timed-event series that helps with variable frame-rates. Useful for things that need to happen at a
