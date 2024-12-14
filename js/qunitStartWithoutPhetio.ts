d9895b0d6141c498f290d54a1d73ac307a301e45
phet-dev <phetadmin@gmail.com>
2024-12-14 03:33:01 -0700

    update copyright dates from daily grunt work

--- js/qunitStartWithoutPhetio.ts
+++ js/qunitStartWithoutPhetio.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2024, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 
 /**
  * Start Qunit while all runtime modalities (debugging/headless puppeteer).
