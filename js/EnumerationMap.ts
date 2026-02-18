ea1c4f3f52a2703f5f3f7f359d75e8d38a499344
phet-dev <phetadmin@gmail.com>
2026-02-18 03:59:48 -0700

    update copyright dates from daily grunt work

--- js/EnumerationMap.ts
+++ js/EnumerationMap.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2025, University of Colorado Boulder
+// Copyright 2019-2026, University of Colorado Boulder
 
 /**
  * An object that contains a value for each item in an enumeration.
